4adf6b5f27e2c95ea704630128dc55b23a726109
loretoligu <loretoligu@gmail.com>
2024-10-11 12:32:34 +0200

    includes for header and footer

---
+++ privado/includes/header.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="container_header">
+        <nav class="nav_top">
+            <a href="main.html">
+                <img class="logo" src="img/Logo-tienda.jpg" alt="logo">
+            </a>  
+            <div class="navigator">CATÁLOGO</div>   
+            <div class="navigator">USUARIO</div>  
+            <div class="navigator">SOPORTE</div>      
+            <div class="navigator">VISTA CLIENTE</div>   
+        </nav>
+        <div class="nav_top">
+            <div class="search">   
+                <button id="search_button">
+                    <span class="material-icons">search</span>
+                </button>
+                <button id="search_button_bar">
+                    <span class="material-icons">search</span>
+                </button>                
+                <input type="text" name="search_bar" placeholder="Buscar..." id="search_bar">
+                <button id="close_search_bar">
+                    <span class="material-icons">close</span>
+                </button> 
+            </div>              
+            <div class="navigator">CERRAR SESIÓN</div> 
+        </div>
+    </div>
+</header>
---
+++ privado/main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camisetas</title>
+
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/footer.css">
+    <script src="js/main.js"></script>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300..900&display=swap" rel="stylesheet">
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <?php
+        include"includes/header.php";
+    ?>
+    <section>
+        <div class="container_section">
+            <div class="container_content"></div>
+        </div>       
+    </section>
+    <?php
+        include"includes/footer.php";
+    ?>
+</body>
+</html>
